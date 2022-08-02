7063bfc7c34c2fc6dc2a896657766767f1a4f53c
risephoenix <>
2022-08-02 15:14:53 +0300

    multiple changes: removed optuna and changes in plot_clusters

--- eeg_lib.py
+++ eeg_lib.py
@@ -10,12 +10,10 @@ from mne.stats import permutation_cluster_test
 from mne.time_frequency import psd_multitaper
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import balanced_accuracy_score, roc_curve, auc, RocCurveDisplay
-from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, minmax_scale
 from tqdm import tqdm
-import optuna
-import optuna.integration.lightgbm as lgb
 from lightgbm import LGBMClassifier as lgbm
 
 
@@ -218,21 +216,20 @@ def predict_lgbm(data, eeg_param, ps=None):
     :rtype: tuple
     :return: ac, roc_auc, interp_tpr, feature_importances, y_predict_pr
     """
-    mean_fpr, st = np.linspace(0, 1, 100), None
-    params = {'objective': 'binary', 'metric': 'binary_error', 'verbosity': -1, 'boosting_type': 'gbdt', 'seed': 42}
-    study_tuner = optuna.create_study(direction='minimize')
-    if ps is None:
-        ps = RepeatedKFold(n_splits=10, n_repeats=10, random_state=42)
+    mean_fpr = np.linspace(0, 1, 100)
     sc = StandardScaler()
     data[0] = sc.fit_transform(data[0])
     data[1] = sc.transform(data[1])
-    train = lgb.Dataset(data[0], label=data[2])
-    tuner = lgb.LightGBMTunerCV(params, train, study=study_tuner, verbose_eval=False,
-                                seed=42, folds=ps, num_boost_round=1000)
-    tuner.run()
-    model = lgbm(tuner.best_params)
+    fit_params = {'objective': 'binary'}
+    m = lgbm(**fit_params)
+    param_grid = {'reg_alpha': list(np.power(10.0, np.arange(-5, 5))),
+                  'reg_lambda': list(np.power(10.0, np.arange(-5, 5)))}
+    if ps is None:
+        ps = 5
+    model = GridSearchCV(m, param_grid, cv=ps)
     model.fit(data[0], data[2])
-    feature_importances = ((model.feature_importances_ / sum(model.feature_importances_)) * 100).reshape(
+    feature_importances = ((model.best_estimator_.feature_importances_ / sum(model.best_estimator_
+                                                                             .feature_importances_)) * 100).reshape(
         (len(eeg_param[0]), len(eeg_param[1])))
     y_predict, y_predict_pr = model.predict(data[1]), model.predict_proba(data[1])
     ac = balanced_accuracy_score(data[3], y_predict)
@@ -242,10 +239,30 @@ def predict_lgbm(data, eeg_param, ps=None):
     return ac, roc_auc, interp_tpr, feature_importances, y_predict_pr
 
 
-def c_bar(v_min, v_max, c_map, fig):
+def c_bar(v_min, v_max, c_map, fig, coord=None):
+    """
+    add colorbar to figure
+    Parameters
+    ----------
+    v_min
+        min value
+    v_max
+        max value
+    c_map
+        color map
+    fig: figure
+        figure to add new axes
+    coord: list
+        position of new axes
+    Returns
+    -------
+
+    """
+    if coord is None:
+        coord = [1, 0.3, 0.03, 0.38]
     m = cm.ScalarMappable(cmap=f'{c_map}')
     m.set_array(np.array([v_min, v_max]))
-    cax = fig.add_axes([1, 0.3, 0.03, 0.38])
+    cax = fig.add_axes(coord)
     cb = fig.colorbar(m, cax)
     cb.ax.tick_params(labelsize=40)
 
@@ -258,12 +275,12 @@ def plot_patterns(coefs_list, eeg_param, subfig=None):
     data: list[np.ndarray]
     eeg_param: tuple
         (fr_bands, info)
-    subfig:
+    subfig
     Returns
     -------   
     :rtype:matplotlib.figure.Figure
     """
-    min_max, s, vmax, vmin, ret = [], coefs_list[0].shape, 1, -1, False
+    min_max, s, vmax, vmin, ret, fig = [], coefs_list[0].shape, 1, -1, False, None
     if subfig is None:
         ret = True
         fig, axes = plt.subplots(nrows=2, ncols=len(eeg_param[0]), figsize=(30, 20))
@@ -272,7 +289,6 @@ def plot_patterns(coefs_list, eeg_param, subfig=None):
     for i in range(len(coefs_list)):
         min_max.append(minmax_scale(coefs_list[i].reshape(2, -1), feature_range=(vmin, vmax), axis=1).reshape(s))
     data = np.mean(min_max, axis=0)
-    data = minmax_scale(data.reshape(2, -1), feature_range=(vmin, vmax), axis=1).reshape(s)
     for name, pos, plot_name, ind in zip(('patterns_', 'filters_'), (0.82, 0.5),
                                          ('Patterns', 'Filters'), (0, 1)):
         for i, key in enumerate(list(eeg_param[0].keys())):
@@ -282,7 +298,8 @@ def plot_patterns(coefs_list, eeg_param, subfig=None):
                                    fontdict={'fontsize': 55, 'fontweight': 'semibold'})
             mne.viz.tight_layout()
     if ret:
-        c_bar(vmin, vmax, 'RdBu_r', fig)
+        if fig is not None:
+            c_bar(vmin, vmax, 'RdBu_r', fig)
         return fig
     else:
         return axes
@@ -301,7 +318,7 @@ def plot_feature_importance(data, eeg_param, subfig=None):
     -------   
     figure     
     """
-    vmin, vmax, min_max, s, ret = 0, 1, [], data[0].shape, False
+    vmin, vmax, min_max, s, ret, fig = 0, 1, [], data[0].shape, False, None
     if subfig is None:
         fig, axes = plt.subplots(nrows=1, ncols=len(eeg_param[0]), figsize=(30, 20))
         ret = True
@@ -310,7 +327,6 @@ def plot_feature_importance(data, eeg_param, subfig=None):
     for i in range(len(data)):
         min_max.append(minmax_scale(data[i].reshape(1, -1), feature_range=(0, 1), axis=1).reshape(s))
     data = np.mean(min_max, axis=0)
-    data = minmax_scale(data.reshape(1, -1), feature_range=(0, 1), axis=1).reshape(s)
     for i, key in enumerate(list(eeg_param[0].keys())):
         mne.viz.plot_topomap(data[i, :], eeg_param[1], vmin=0, vmax=1, axes=axes[i], cmap='hot',
                              show=False)
@@ -318,30 +334,36 @@ def plot_feature_importance(data, eeg_param, subfig=None):
                           fontdict={'fontsize': 55, 'fontweight': 'semibold'})
         mne.viz.tight_layout()
     if ret:
-        c_bar(vmin, vmax, 'hot', fig)
+        if fig is not None:
+            c_bar(vmin, vmax, 'hot', fig)
+        return fig
     else:
         return axes
 
 
-def plot_clusters(data, cl_param):
+def plot_clusters(data, l_ind, cl_param):
     """
     function that plot significant clusters on topo
     Parameters
     ----------
-    data: list[np.ndarray]
-        list with samples
+    data: np.ndarray
+        samples
+    l_ind: list[list]
+        groups indexes
     cl_param: tuple
         (fr_bands, info)
     Returns
     -------
-    figure
+    :rtype:matplotlib.figure.Figure
     """
     fig, axes = plt.subplots(nrows=3, ncols=len(cl_param[0]), figsize=(30, 20))
-    vmin, thr = 0, -stats.t.ppf(0.05/2, 1)
-    for i, name in zip(range(3), ['A', 'B', 'C']):
-        t_obs, clusters, cluster_p_values, _ = permutation_cluster_test([data[0][:, i], data[1][:, i]], threshold=thr,
+    vmin, thr, res_ar = 0, -stats.t.ppf(0.05 / 2, 1), np.zeros(data[0, 0].shape)
+    for i, co, name in zip(range(3), [0.1, 0.38, 0.66], ['A', 'B', 'C']):
+        t_obs, clusters, cluster_p_values, _ = permutation_cluster_test([data[l_ind[0], i, ...],
+                                                                         data[l_ind[1], i, ...]],
+                                                                        threshold=thr,
                                                                         adjacency=None, out_type='mask',
-                                                                        n_permutations=1024)
+                                                                        n_permutations=1024, verbose=False)
         t_obs_plot = np.nan * np.ones_like(t_obs)
         for c, p_val in zip(clusters, cluster_p_values):
             if p_val <= 0.05:
@@ -352,20 +374,19 @@ def plot_clusters(data, cl_param):
         mr_ar = res_ar > 0
         vmax = np.amax(res_ar)
         for j, key in enumerate(list(cl_param[0].keys())):
-            mne.viz.plot_topomap(axes=axes[i, j], time_format=None, colorbar=False, times=[0], vmin=vmin, vmax=vmax,
-                                 size=5, show_names=True, show=False, cmap='hot',
-                                 mask=mr_ar[i, :].reshape(-1, 1))
+            mne.viz.plot_topomap(res_ar[j], cl_param[1], vmin=vmin, vmax=vmax, axes=axes[i, j],
+                                 show=False, cmap='hot', mask=mr_ar[j].reshape(-1, 1))
             axes[i, j].set_title(label=f'{cl_param[0][key][0]}-{cl_param[0][key][1]} Hz',
                                  fontdict={'fontsize': 50, 'fontweight': 'semibold'})
             mne.viz.tight_layout()
-        c_bar(vmin, vmax, 'hot', fig)
-        fig.text(-0.05, 0.5, f'{name}', fontdict={'fontsize': 90, 'fontweight': 'semibold'})
+            c_bar(vmin, vmax, 'hot', fig, coord=[1, co, 0.03, 0.25])
+            axes[i, 0].text(-0.2, 0, f'{name}', fontdict={'fontsize': 50, 'fontweight': 'semibold'})
     return fig
 
 
 def plot_roc_curves(tprs, aucs, list_predictions, list_y_true, plot_all=True, ax=None):
     """
-
+    function that plot roc curves
     Parameters
     ----------
     tprs: list[float]
@@ -379,9 +400,9 @@ def plot_roc_curves(tprs, aucs, list_predictions, list_y_true, plot_all=True, ax
     ax:
     Returns
     -------
-    figure
+    :rtype:matplotlib.figure.Figure
     """
-    mean_fpr, ret = np.linspace(0, 1, 100), False
+    mean_fpr, ret, fig = np.linspace(0, 1, 100), False, None
     if ax is None:
         fig, ax = plt.subplots(figsize=(12, 8))
         ret = True
@@ -403,13 +424,31 @@ def plot_roc_curves(tprs, aucs, list_predictions, list_y_true, plot_all=True, ax
     ax.set_ylabel('False Positive Rate', fontsize=70)
     ax.legend(loc='lower right', fontsize=55)
     if ret:
-        plt.close(fig)
         return fig
     else:
         pass
 
 
 def merge_fig(fig_type, data, s_ind, metrics, results, settings, inf, roc_plot_all=False, pl=True):
+    """
+    merge several figures
+
+    Parameters
+    ----------
+    fig_type
+    data: ndarray
+    s_ind
+    metrics: dict
+    results: ndarray
+    settings
+    inf
+    roc_plot_all: bool
+    pl: bool
+
+    Returns
+    -------
+    :rtype:matplotlib.figure.Figure
+    """
     if fig_type not in [1, 2]:
         raise ValueError('Please type correct value: 1 for filters/patterns, 2 for feature importance')
     fig = plt.figure(constrained_layout=True, figsize=(75, 60))
@@ -427,6 +466,7 @@ def merge_fig(fig_type, data, s_ind, metrics, results, settings, inf, roc_plot_a
                 m = cm.ScalarMappable(cmap='RdBu_r')
                 m.set_array(np.array([-1, 1]))
                 cb = subfigs[0].colorbar(m, ax=ax_bar, shrink=0.6, location='bottom')
+                cb.ax.tick_params(labelsize=80)
         if fig_type == 2:
             ax_bar = plot_feature_importance(list(compress(data[s_ind, n, ...], results[s_ind, n, 1] > 0.5)),
                                              [settings.fr_bands, inf], ax)
@@ -434,7 +474,7 @@ def merge_fig(fig_type, data, s_ind, metrics, results, settings, inf, roc_plot_a
                 m = cm.ScalarMappable(cmap='hot')
                 m.set_array(np.array([0, 1]))
                 cb = subfigs[0].colorbar(m, ax=ax_bar, shrink=0.6, pad=0.3, location='bottom')
-    cb.ax.tick_params(labelsize=80)
+                cb.ax.tick_params(labelsize=80)
     for n, (ax, name) in enumerate(zip(axs_right, ['D', 'E', 'F'])):
         bars_l[1].update(1)
         ax.text(-0.05, 0.5, f'{name}', fontdict={'fontsize': 90, 'fontweight': 'semibold'})
